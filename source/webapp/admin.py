cf11a1a084fcce09c0af42d994b28165f7752b1d
samat_sultanov <samatsayak@gmail.com>
2022-11-12 12:25:17 +0600

    создал файл webapp/teplates/css/style.css для стилей; создал модель GuestBook, зарегистрировал ее в админ панели; создал миграцию и накатил ее; создал записи и сохранил их в созданную фикстуру в webapp/fixtures/dump.json

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import GuestBook
 
-# Register your models here.
+
+class AdminGuestBook(admin.ModelAdmin):
+    list_display = ["id", "author", "status"]
+    list_display_links = ["author"]
+    list_filter = ["status"]
+    search_fields = ["author", "text"]
+    fields = ["author", "email", "status", "text", "created_at", "updated_at"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
+admin.site.register(GuestBook, AdminGuestBook)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+
+class GuestBook(models.Model):
+    author = models.CharField(max_length=50, null=False, blank=False, verbose_name="Автор")
+    email = models.EmailField(max_length=50, null=False, blank=False, verbose_name="Почта")
+    text = models.TextField(max_length=500, null=False, blank=False, verbose_name="Текст Записи")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Время редактирования")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0],
+                              verbose_name='Статус')
+
+    def __str__(self):
+        return f"Запись {self.pk}: {self.author}"
